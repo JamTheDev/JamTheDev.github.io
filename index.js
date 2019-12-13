b5ee617bab2b63b48f440233dd1f729328f32fc5
JamTheDev <2004jamvillarosa@gmail.com>
2019-12-13 14:47:59 +0800

    First website upload

---
+++ index.js
@@ -0,0 +1,62 @@
+var text = document.getElementById("text");
+var button = document.getElementById("button");
+var head = document.getElementById("head");
+var head1 = document.getElementById("head1");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdn9V1g8kfGBCadJSuvxK89F0dBWWE9F4",
+  authDomain: "delpiapp05.firebaseapp.com",
+  databaseURL: "https://delpiapp05.firebaseio.com",
+  projectId: "delpiapp05",
+  storageBucket: "delpiapp05.appspot.com",
+  messagingSenderId: "268595622844",
+  appId: "1:268595622844:web:bc6459832109c90875b1b9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+database = firebase.database();
+
+var ref = database.ref("bullyReport");
+
+ref.on('value', gotData, errData);
+
+function gotData(data){
+ var object = data.val();
+
+var nameId1 = Object.keys(object);
+
+for (var i = 0 ; i < nameId1.length ; i++){
+  var k = nameId1[i];
+
+  var name = object[k].name;
+  var grade =object[k].grade;
+  var nameId = object[k].nameId;
+  var offenses = object[k].offenses;
+  var report = object[k].report;
+  var section = object[k].section;
+  console.log(nameId, name, grade, offenses, report, section);
+  var li = document.createElement("li");
+  li.innerHTML = "grade: " + grade +' / ' + "name: " + name + ' / ' + "nameId: " + nameId +' / ' + "type of offense: " + offenses + ' / ' + "report :" + report + ' / ' + "section: " + section;
+  document.body.appendChild(li);   
+}
+
+}
+function errData(err){
+  console.log("err");
+  console.log(err);
+}
+var f1 = firebaseRef = firebase.database().ref().child("bullyReport");
+f1.on(`value`, function (datasnapshot){
+  head1.innerText = datasnapshot.val();
+
+});
+
+function myclick() {
+    var val = text.value;
+    window.alert(val);
+        
+    }
+
