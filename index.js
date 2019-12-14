807ffdaf09feee90932a321178421f1376f3e8d8
JamTheDev <2004jamvillarosa@gmail.com>
2019-12-14 11:21:10 +0800

    THIRD AND FINAL COMMIT (unless someone designs this web which is unnecessary)

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var text = document.getElementById("text");
 var button = document.getElementById("button");
 var head = document.getElementById("head");
 var head1 = document.getElementById("head1");
-
-const firebaseConfig = {
+const firebaseConfig = {  
+  
   apiKey: "AIzaSyCdn9V1g8kfGBCadJSuvxK89F0dBWWE9F4",
   authDomain: "delpiapp05.firebaseapp.com",
   databaseURL: "https://delpiapp05.firebaseio.com",
@@ -24,6 +24,12 @@ var ref = database.ref("bullyReport");
 ref.on('value', gotData, errData);
 
 function gotData(data){
+
+  var namesIds = document.getElementsByClassName("Jam");
+
+  for (var i = 0 ; i < namesIds.length ; i + 1){
+    namesIds[i].remove();
+  }
  var object = data.val();
 
 var nameId1 = Object.keys(object);
@@ -32,26 +38,42 @@ for (var i = 0 ; i < nameId1.length ; i++){
   var k = nameId1[i];
 
   var name = object[k].name;
+
   var grade =object[k].grade;
+
   var nameId = object[k].nameId;
+
   var offenses = object[k].offenses;
+
   var report = object[k].report;
+
   var section = object[k].section;
+
   console.log(nameId, name, grade, offenses, report, section);
 
-
   var li = document.createElement("li");
+
   li.innerHTML = "grade: " + grade +' / ' + "name: " + name + ' / ' + "nameId: " + nameId +' / ' + "type of offense: " + offenses + ' / ' + "report :" + report + ' / ' + "section: " + section;
+
+  li.className = "Jam";
+
   document.body.appendChild(li);
+
 }
 
 }
+
 function errData(err){
+
   console.log("err");
+
   console.log(err);
+
 }
 function myclick() {
+
     var val = text.value;
+    
     window.alert(val);
     }
 
